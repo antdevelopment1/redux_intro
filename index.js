15b2ce1e57b357f8460cedefbcb1ee7a39778756
April Copes <antdevelopment1@gmail.com>
2018-12-12 20:19:27 -0500

    Adds dummy example of reducers

--- index.js
+++ index.js
@@ -40,3 +40,39 @@ const createClaim = (name, amountOfMoneyToCollect) => {
         }
     }
 }
+
+
+// ========
+// REDUCERS
+// ========
+// We need to default the oldListOfClaims array in case this is the first time we call this function and there are no exsisting claims to pass
+// in case it isnt a create claim
+const claimHistory = (oldlistOfClaims = [], action) => {
+    if (action.type === 'CREATE_CLAIM') {
+        return [...oldlistOfClaims, action.payload];
+    }
+
+    return oldlistOfClaims;
+}
+
+const accounting = (bagOfMoney, action) => {
+
+    if (action.type === 'CREATE_CLAIM') {
+        return bagOfMoney - action.payload.amountOfMoneyToCollect;
+    } else if (action.type === 'CREATE_POLICY') {
+        return bagOfMoney + action.payload.amount;
+    }
+     return bagOfMoney;
+
+}
+
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === 'CREATE_POLICY') {
+        return [...listOfPolicies, action.payload.name]
+    } else if (action.type === 'DELETE_POLICY') {
+        return listOfPolicies.filter(name => { 
+            name !== action.payload.name;
+        })
+    }
+    return listOfPolicies;
+}
