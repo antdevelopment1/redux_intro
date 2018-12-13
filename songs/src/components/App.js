74d056d733e20403e7a0418e9ad401be58beab74
April Copes <antdevelopment1@gmail.com>
2018-12-13 12:57:20 -0500

    Adds in action files and ties in simple function

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import actions from './../actions/index.js';
+// The path for the line above and below are equivalent. Both will navigate to actions/index.js
+import {selectSong} from './../actions';
 
 const App = () => {
     return (
