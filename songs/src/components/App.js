e854c4f884e086113ea7e436e0179d3f216dad96
April Copes <antdevelopment1@gmail.com>
2018-12-17 07:02:20 -0500

    styles song list using sematic ui

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import actions from './../actions/index.js';
 // The path for the line above and below are equivalent. Both will navigate to actions/index.js
-import {selectSong} from './../actions';
+// import {selectSong} from './../actions';
+import SongList from './SongList';
 
 const App = () => {
     return (
-        <div>Hello I am App</div>
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <div><SongList /></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
